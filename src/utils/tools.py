2ddf679829d64ebeaa45afb85c0b1d9783d43e67
Matheus Marinho <temarinho76@gmail.com>
2025-07-22 09:57:26 -0300

    udpate: local_path funcion to get absolute path

--- src/utils/tools.py
+++ src/utils/tools.py
@@ -1,7 +1,8 @@
+import os
 import sys
 from pathlib import Path
 
 def local_path():
     if getattr(sys, 'frozen', False) and hasattr(sys, '_MEIPASS'):
         return Path(sys._MEIPASS)
-    return Path(".")
+    return Path(os.path.abspath("."))
